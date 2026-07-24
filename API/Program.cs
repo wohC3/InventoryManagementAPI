2262c1fd8ba9a134ada42183285ef138449e6a9e
wohC3 <220244608+wohC3@users.noreply.github.com>
2026-07-24 13:08:04 +0300

    Add: Apply EF Core Migrations on application startup

--- API/Program.cs
+++ API/Program.cs
@@ -66,6 +66,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
       });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
